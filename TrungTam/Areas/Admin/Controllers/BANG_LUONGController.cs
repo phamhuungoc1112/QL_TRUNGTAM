becb4454af631b2083b9ac7af874d9c7373f2114
phamhuungoc050798 <phamhuungoc050798@gmail.com>
2019-07-05 14:09:17 +0700

    VERSION1

---
+++ TrungTam/Areas/Admin/Controllers/BANG_LUONGController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using TrungTam.Areas.Admin.Models;
+
+namespace TrungTam.Areas.Admin.Controllers
+{
+    public class BANG_LUONGController : Controller
+    {
+        private QL_TRUNGTAMEntities1 db = new QL_TRUNGTAMEntities1();
+
+        // GET: Admin/BANG_LUONG
+        public ActionResult Index()
+        {
+            return View(db.BANG_LUONG.ToList());
+        }
+
+        // GET: Admin/BANG_LUONG/Details/5
+        public ActionResult Details(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BANG_LUONG bANG_LUONG = db.BANG_LUONG.Find(id);
+            if (bANG_LUONG == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bANG_LUONG);
+        }
+
+        // GET: Admin/BANG_LUONG/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Admin/BANG_LUONG/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "MA_LOAI_LUONG,TEN_LOAI,DON_GIA")] BANG_LUONG bANG_LUONG)
+        {
+            if (ModelState.IsValid)
+            {
+                bANG_LUONG.MA_LOAI_LUONG = Guid.NewGuid();
+                db.BANG_LUONG.Add(bANG_LUONG);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(bANG_LUONG);
+        }
+
+        // GET: Admin/BANG_LUONG/Edit/5
+        public ActionResult Edit(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BANG_LUONG bANG_LUONG = db.BANG_LUONG.Find(id);
+            if (bANG_LUONG == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bANG_LUONG);
+        }
+
+        // POST: Admin/BANG_LUONG/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "MA_LOAI_LUONG,TEN_LOAI,DON_GIA")] BANG_LUONG bANG_LUONG)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(bANG_LUONG).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(bANG_LUONG);
+        }
+
+        // GET: Admin/BANG_LUONG/Delete/5
+        public ActionResult Delete(Guid? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            BANG_LUONG bANG_LUONG = db.BANG_LUONG.Find(id);
+            if (bANG_LUONG == null)
+            {
+                return HttpNotFound();
+            }
+            return View(bANG_LUONG);
+        }
+
+        // POST: Admin/BANG_LUONG/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(Guid id)
+        {
+            BANG_LUONG bANG_LUONG = db.BANG_LUONG.Find(id);
+            db.BANG_LUONG.Remove(bANG_LUONG);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
